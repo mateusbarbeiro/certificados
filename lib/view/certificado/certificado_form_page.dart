685a4b78e096c3c4b44fd6f09e873456f48dd937
Francielxyz <ruanvha15@gmail.com>
2022-07-13 22:42:24 -0300

    criação de componetes de form

---
+++ lib/view/certificado/certificado_form_page.dart
@@ -0,0 +1,9 @@
+import 'package:certificados/view/certificado/widget/certificado_form_widget.dart';
+import 'package:flutter/material.dart';
+
+class CertificadoFormPage extends StatefulWidget {
+  const CertificadoFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<CertificadoFormPage> createState() => CertificadoPageWidget();
+}
