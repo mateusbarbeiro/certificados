ec565f81190749d234f1fdfce897542b30ddbc56
mateus barbeiro <mateus_garcia01@hotmail.com>
2022-07-20 22:09:15 -0300

    ajustes

---
+++ lib/view/certificado/widget/certificado_detail_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../certificado_detail_page.dart';
+
+class CertificadoDetailWidget extends State<CertificadoDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
